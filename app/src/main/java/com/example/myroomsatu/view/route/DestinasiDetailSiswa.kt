3de5d743686b0fc0b9f5e6986dcc74216bcacf07
dzakyputra45 <dzakypratama45@gmail.com>
2025-12-05 22:08:11 +0700

    Tambahkan DestinasiDetail siswa untuk melihat detail siswa

---
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,12 @@
+package com.example.myroomsatu.view.route
+
+import com.example.myroomsatu.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi(
+    route = "detail_siswa",
+    titleRes = R.string.detail_siswa,
+    itemIdArg = "idSiswa",
+    routeWithArgs = "detail_siswa/{idSiswa}"
+)
+
+
