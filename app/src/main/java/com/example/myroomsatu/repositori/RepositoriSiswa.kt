a1c362caa9e1a317c6122e43864b10e6889908ed
dzakyputra45 <dzakypratama45@gmail.com>
2025-12-05 22:03:58 +0700

    Tambahkan fungsi update dan delete di RepositoriSiswa.kt

--- app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
@@ -7,16 +7,22 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    fun getSiswaStream(id:Int): Flow<Siswa?>
     suspend fun insertSiswa(siswa: Siswa)
+
+    suspend fun deleteSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
     private val siswaDAO: SiswaDao
 ) : RepositoriSiswa {
 
-    override fun getAllSiswaStream(): Flow<List<Siswa>> =
-        siswaDAO.getAllSiswa()
-
-    override suspend fun insertSiswa(siswa: Siswa) =
-        siswaDAO.insertSiswa(siswa)
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDAO.getAllSiswa()
+    override fun getSiswaStream(id:Int): Flow<Siswa?> = siswaDAO.getSiswa(id)
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDAO.insertSiswa(siswa)
+    override suspend fun deleteSiswa(siswa : Siswa)= siswaDAO.delete(siswa)
+    override suspend fun updateSiswa(siswa : Siswa)= siswaDAO.update(siswa)
 }
